665d2d94bad86de8e4b018a3cad343c84c6a1d0d
ankit0706 <46675158+ankit0706@users.noreply.github.com>
2020-06-29 14:14:42 +0530

    tested for multi partition, multi consumers

--- src/main/java/com/sample/demo/KafkaController.java
+++ src/main/java/com/sample/demo/KafkaController.java
@@ -15,5 +15,17 @@ public class KafkaController {
 		producer.sendMessage(message);
 		return "Data pushed on Kafka topic";
     }
+	
+	@PostMapping(value = "/publish2")
+    public String sendMessageToKafkaTopic2(@RequestBody String message) {
+		producer.sendMessageToMultiPart(message);
+		return "Data pushed on multi part Kafka topic";
+    }
+	
+	@PostMapping(value = "/publish3")
+    public String sendMessageTosimpleTopic(@RequestBody String message) {
+		producer.sendMessageToSimple(message);
+		return "Data pushed on Kafka topic named test";
+    }
 
 }
--- src/main/java/com/sample/demo/Producer.java
+++ src/main/java/com/sample/demo/Producer.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class Producer {
 	    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
 	    private static final String TOPIC = "dataFiles";
+	    private static final String MULTI_PART_TOPIC = "multipart";
 
 	    @Autowired
 	    private KafkaTemplate<String, String> kafkaTemplate;
@@ -17,6 +18,16 @@ public class Producer {
 	    public void sendMessage(String data) {
 	        this.kafkaTemplate.send(TOPIC, data);
 	        logger.info("data sent on topic");
+	    }
+	    
+	    public void sendMessageToMultiPart(String data) {
+	        this.kafkaTemplate.send(MULTI_PART_TOPIC, data);
+	        logger.info("data sent on multi part topic");
+	    }
+	    
+	    public void sendMessageToSimple(String data) {
+	        this.kafkaTemplate.send("test", data);
+	        logger.info("data sent on topic called test");
 	    }
 
 }
